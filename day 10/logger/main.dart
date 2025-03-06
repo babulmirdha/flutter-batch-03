0e2bcc5981e4a5ddea5824767acba5e978b9cb27
babulout <babul.mirdha@outlook.com>
2025-03-06 13:06:08 +0600

    added

--- day 10/logger/main.dart
+++ day 10/logger/main.dart
@@ -3,6 +3,11 @@ import 'ConsoleLogger.dart';
 import 'FileLogger.dart';
 
 void main(List<String> args) {
-  var app = App(logger: ConsoleLogger());
-  app.run();
+  // Console Logger
+  var app1 = App(logger: ConsoleLogger());
+  app1.run();
+
+// File Logger
+  var app2 = App(logger: FileLogger());
+  app2.run();
 }
